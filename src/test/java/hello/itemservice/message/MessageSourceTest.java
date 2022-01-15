003f7e51753feb8e97c542660e19d648de82143c
azurealstn <azurealstn@naver.com>
2022-01-15 12:27:19 +0900

    웹 어플리케이션에 간단하게 국제화 적용하기!

---
+++ src/test/java/hello/itemservice/message/MessageSourceTest.java
@@ -0,0 +1,60 @@
+package hello.itemservice.message;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.MessageSource;
+import org.springframework.context.NoSuchMessageException;
+
+import java.util.Locale;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@SpringBootTest
+public class MessageSourceTest {
+
+    @Autowired
+    private MessageSource messageSource;
+
+    @Test
+    void hello_message() {
+        String result = messageSource.getMessage("hello", null, null);
+        assertThat(result).isEqualTo("안녕");
+    }
+
+    @Test
+    void notFoundMessageCode() {
+        assertThatThrownBy(() -> messageSource.getMessage("no_code", null, null))
+                .isInstanceOf(NoSuchMessageException.class);
+    }
+
+    @Test
+    void notFoundMessageCodeDefaultMessage() {
+        String result = messageSource.getMessage("no_code", null, "기본 메시지", null);
+        assertThat(result).isEqualTo("기본 메시지");
+    }
+
+    @Test
+    void argumentMessage() {
+        String result = messageSource.getMessage("hello.name", new Object[]{"Spring"}, null);
+        assertThat(result).isEqualTo("안녕 Spring");
+    }
+
+    @Test
+    void defaultLang() {
+        String result1 = messageSource.getMessage("hello", null, null);
+        String result2 = messageSource.getMessage("hello", null, Locale.KOREA);
+
+        assertThat(result1).isEqualTo("안녕");
+        assertThat(result2).isEqualTo("안녕");
+    }
+
+    @Test
+    void enLang() {
+        String result = messageSource.getMessage("hello", null, Locale.ENGLISH);
+
+        assertThat(result).isEqualTo("hello");
+    }
+}
